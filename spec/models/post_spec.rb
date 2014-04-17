26f9b35fcf583d2c9fdeacee59183672c7c1eb42
ruan-brandao <ruan.bernardo@gmail.com>
2014-04-16 23:49:20 -0300

    Testando relacionamentos entre post e user

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,4 +4,6 @@ describe Post do
   it { should validate_presence_of(:content) }
   it { should validate_numericality_of(:likes) }
   it { should validate_numericality_of(:dislikes) }
+
+  it { should belong_to(:user) }
 end
