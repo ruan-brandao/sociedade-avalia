ed005159eac761a71d8de2886b0598e362f437d0
ruan-brandao <ruan.bernardo@gmail.com>
2014-09-15 19:06:23 -0300

    Fazendo testes de Pages Controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822023942) do
+ActiveRecord::Schema.define(version: 20140915222259) do
+
+  create_table "comments", force: true do |t|
+    t.string   "text"
+    t.decimal  "likes"
+    t.decimal  "dislikes"
+    t.decimal  "user_id"
+    t.decimal  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.text     "content"
