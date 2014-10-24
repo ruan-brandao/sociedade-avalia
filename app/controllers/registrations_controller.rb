b390bccb85cd89d20bf6e885833dc18b3b468172
ruan-brandao <ruan.bernardo@gmail.com>
2014-10-24 02:00:13 -0200

    Adicionando tela para editar perfil

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -22,14 +22,16 @@ class RegistrationsController < Devise::RegistrationsController
 	private
 
 	def sign_up_params
-		params.require(:user).permit(:first_name, :last_name, :birth_date, :gender, :city, :state, 
-																 :username, :email, :password, :password_confirmation,
-																 :profile_picture)
+		params.require(:user).permit(:first_name, :last_name, :birth_date, :gender, 
+																 :city, :state, :username, :email, :password, 
+																 :password_confirmation,
+																 :profile_picture, :political_party_id)
 	end
 
 	def account_update_params
-		params.require(:user).permit(:first_name, :last_name, :birth_date, :gender, :city, :state, 
-																 :username, :email, :password, :password_confirmation,
-																 :profile_picture)
+		params.require(:user).permit(:first_name, :last_name, :birth_date, :gender, 
+																 :city, :state, :username, :email, :password, 
+																 :password_confirmation,
+																 :profile_picture, :political_party_id)
 	end
 end
