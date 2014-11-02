e08b71a930588c6c69d16bc701e156f6ba7ae24a
ruan-brandao <ruan.bernardo@gmail.com>
2014-11-01 23:56:06 -0200

    Dropando tabela de comentários

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141101235545) do
-
-  create_table "comments", force: true do |t|
-    t.string   "text"
-    t.decimal  "likes"
-    t.decimal  "dislikes"
-    t.decimal  "user_id"
-    t.decimal  "post_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20141102015351) do
 
   create_table "political_parties", force: true do |t|
     t.string   "name"
