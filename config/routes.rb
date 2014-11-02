293c507b7a87a4c3ab77104b4ebec1401c52e205
ruan-brandao <ruan.bernardo@gmail.com>
2014-11-01 22:29:27 -0200

    Alterando rota para partidos políticos

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ SociedadeAvalia::Application.routes.draw do
   get "users/:id/turn_to_elector", to: "profiles#turn_to_elector"
 
   resources :posts
-  resources :political_parties
+  resources :political_parties, path: 'partidos'
 
   resources :relationships, only: [:create, :destroy]
 
@@ -34,53 +34,4 @@ SociedadeAvalia::Application.routes.draw do
   get '/:id', to: 'profiles#show'
   get '/:id/following', to: 'users#following'
   get '/:id/followers', to: 'users#followers'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
