d10123996f4994549d0fa536f03f1dcd0776ef3e
ruan-brandao <ruan.bernardo@gmail.com>
2014-11-03 22:56:20 -0200

    Refatorando contador de likes e dislikes de political_party

--- app/models/political_party.rb
+++ app/models/political_party.rb
@@ -5,4 +5,12 @@ class PoliticalParty < ActiveRecord::Base
 	validates :name, :siglum, :number, :position, presence: true
 	validates :siglum, length: { maximum: 10 }
 	validates :number, numericality: { only_integer: true, greater_than: 0, less_than: 100 }
+
+	def likes
+		PoliticalPartyLike.where(political_party_id: self.id, value:true).count
+	end
+
+	def dislikes
+		self.political_party_likes.count -  PoliticalPartyLike.where(political_party_id: self.id, value:true).count
+	end
 end
