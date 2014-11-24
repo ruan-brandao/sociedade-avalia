ba35c773280cc37c26457d6e4f6c0dc087b5abcd
ruan-brandao <ruan.bernardo@gmail.com>
2014-11-24 16:09:45 -0200

    Adicionando confirmable a usuários

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111015939) do
+ActiveRecord::Schema.define(version: 20141124180411) do
 
   create_table "political_parties", force: true do |t|
     t.string   "name"
