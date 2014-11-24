d30b03429943cc1e42ebfd16d31898488706d870
ruan-brandao <ruan.bernardo@gmail.com>
2014-11-24 16:13:27 -0200

    Configurando ambientes de desenvolvimento e produção para o envio de emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,13 @@ SociedadeAvalia::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {:host => 'sociedadeavalia.com.br'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => 25,
+    :domain  => 'yourdomain.com'
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
